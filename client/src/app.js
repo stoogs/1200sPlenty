f1d02021310171b0900750b6b0c5bbe2a6b33b5d
Stuart Black <StuartBlack@gmail.com>
2018-12-12 13:25:19 +0000

    logo added

--- client/src/app.js
+++ client/src/app.js
@@ -3,8 +3,9 @@ const FormView = require('./views/form_view.js')
 document.addEventListener('DOMContentLoaded', () => {
     console.log('JavaScript loaded from bundle.js to app.js');
 
+    //Get value from submitted form
     const formView = new FormView();
     formView.bindEvents();
-
+  
 
   });
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 npm run server:dev
 
 this loads nodemon app.js
+
+requires
+
+npm install
+npm install express webpack webpack-cli nodemon
+npm run build
+npm start
