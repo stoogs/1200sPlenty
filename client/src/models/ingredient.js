eddbb94fd0f589902df3e999695a61fbf14e3c81
Stuart Black <StuartBlack@gmail.com>
2018-12-13 14:53:32 +0000

    Before brnach to change to result view

--- client/src/models/ingredient.js
+++ client/src/models/ingredient.js
@@ -4,23 +4,20 @@ const ApiKeys = require('../../public/media/apikeys/apikeys.js');
 
 const Ingredient = function () {
     this.data = null;
-    this.ingredient = "emptyyyy";
+    this.ingredient = "empty";
     this.Api_Id = ApiKeys.api_id;
-    this.Api_Key = ApiKeys.api_key
-
+    this.Api_Key = ApiKeys.api_key 
   };
   
   Ingredient.prototype.bindEvents = function(searched) {
     PubSub.subscribe('IngredientForm:inputtedText', (event) => {
         let sortedIngredient = event.detail;
-            console.log(sortedIngredient)
-            let arraySortedIngredient = sortedIngredient.split(' ');
-            console.log(arraySortedIngredient)
-            let quantity = arraySortedIngredient[0];
-            console.log("quantity", quantity)
-            let onlyIngredients = arraySortedIngredient.slice(1).join(' ')            
-            console.log(onlyIngredients)
-        
+        let arraySortedIngredient = sortedIngredient.split(' ');
+        let quantity = arraySortedIngredient[0];
+        console.log("quantity", quantity)
+        let onlyIngredients = arraySortedIngredient.slice(1).join(' ')            
+        console.log(onlyIngredients) 
+
         this.ingredient = sortedIngredient;
         this.getIngredient(this.ingredient);
         
@@ -36,10 +33,6 @@ const Ingredient = function () {
             this.data = data;
             //publish ingredients to results view
             PubSub.publish('Ingredient:api-results', this.data);
-            //Prove ingredients are here.
-            // console.log('Ingredients API Results...' , this.data);
-            // console.log('Ingredients API calories...' , this.data.calories);
-
         })
         .catch((message) => {
             console.error(message);
