ebb2fd247c3aa06e8d5aee3c044fb9df95467fbd
Stuart Black <StuartBlack@gmail.com>
2018-12-13 12:21:44 +0000

    CSS changes

--- client/src/views/result_view.js
+++ client/src/views/result_view.js
@@ -11,23 +11,32 @@ ResultView.prototype.bindEvents = function () {
     console.log("Result view bindEvents Function");
     PubSub.subscribe('Ingredient:api-results', (event) => {
         this.ingredientFound = event.detail; 
-        console.log('The ingredient is here! from results view', this.ingredientFound.calories, this.ingredientFound); 
+        // console.log('The ingredient is here! from results view', this.ingredientFound.calories, this.ingredientFound); 
         this.render()
     });
     PubSub.subscribe('IngredientForm:inputtedText', (event) => {
         this.chosenFood = event.detail
-        console.log('The ingredient is here! from results view', this.chosenFood); 
+        // console.log('The ingredient is here! from results view', this.chosenFood); 
     });
 };
 
 ResultView.prototype.render = function(){
-    this.resultContainer.innerHTML = '';
     let chosenFood = this.chosenFood;
     let calories = this.ingredientFound.calories;
     let weight = this.ingredientFound.totalWeight;
     let dietLabel = this.ingredientFound.dietLabels;
     console.log('---------------------------------');
     console.log( chosenFood,calories,weight,dietLabel);
+
+    this.resultContainer.innerHTML = '';
+    document.getElementById("just-typed").textContent = this.chosenFood;
+    let displayText = `${chosenFood} has ${calories} calories and weighs ${weight}grams`;
+    document.getElementById("results").textContent = ("Results");
+    document.getElementById("search-result").textContent = displayText;
+
+    let searchResult = document.getElementById("search-result");
+  
+
 };
 
 module.exports = ResultView;
